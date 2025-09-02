c325b72fd98bec7e8c0fd1467a6c9f7d9c6aa1c8
Clipperai <166284550+Clipperai@users.noreply.github.com>
2025-09-02 17:39:16 +0530

    Add files via upload

---
+++ html/Day_4/variables.js
@@ -0,0 +1,12 @@
+let name = "Vishu";
+const pi=3.14;
+var age=20;
+
+console.log("check console");
+alert("this is an alert.")
+document.write("directly on page.")
+
+name= prompt("Enter Your name:");
+alert(`Welcome, ${name} !`);
+
+console.log("User entered:", name)
---
+++ html/Day_5/operators.js
@@ -0,0 +1,9 @@
+let a= 10;
+let b= 5;
+console.log(a+b);
+
+
+let x= "hello"
+let y= "vishu"
+
+console.log(x+y)
